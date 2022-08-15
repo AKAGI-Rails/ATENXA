a2a33e7313c801cdd772ac803a1da7df6e3c3438
AKAGI-Rails <akag99@gmail.com>
2022-08-15 12:32:57 +0900

    refresh around documents

--- make.bat
+++ make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=docs-src\source
-set BUILDDIR=docs-src\build
+set BUILDDIR=docs-src\_build
 set DOCSDIR=docs
 
 if "%1" == "" goto help
